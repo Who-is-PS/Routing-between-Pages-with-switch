cf7ca643c063084c054cae6bd70695ae232cf669
Philipp Schneider <codingphil@web.de>
2023-08-06 16:28:51 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Pricing from './pages/Pricing';
+import About from './pages/About';
+import Home from './pages/home';
 
 function App() {
+  
+  let Component
+  switch(window.location.pathname){
+    case "/":
+      Component = Home
+      break
+    case "/pricing":
+      Component = Pricing
+      break
+    case "/about":
+      Component = About
+      break
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <div className='container'>
+        <Component/>
+      </div>
     </div>
   );
 }
